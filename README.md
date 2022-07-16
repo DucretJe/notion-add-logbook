d576facb854f5e1375da1b6e371964fa59ac4197
DucretJe <jdiphone34@gmail.com>
2022-07-16 17:46:29 +0200

    Add get_id feature

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 This scripts adds a new entry in a Database in Notion.
 The aim is to use it to create a new daily entry in a LogBook.
+
+* Create a Notion integration
+* Share a parent page of the DB with the integration
+
+## How to
+
+* Install dependencies
+
+```sh
+python3 -m pip install -r requirements.txt 
+```
+
+* Run it
+
+```sh
+python3 python/notion_add_entry -t <TOKEN> -n <DB_NAME>
+```
--- python/notion_add_entry/__main__.py
+++ python/notion_add_entry/__main__.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python3
 # coding=utf-8
+
+import argparse
+
+from  get_resource_id import get_id
+
+
+parser = argparse.ArgumentParser(description="Add an entry in a given database")
+parser.add_argument("-t", "--token", help="token", required=True)
+parser.add_argument("-n", "--name", help="name of the database to update", required=True)
+args = parser.parse_args()
+
+# pylint: disable=invalid-name
+if __name__ == "__main__":  # pragma: no cover
+    token = args.token
+    name = args.name
+    print(get_id(token, name))
