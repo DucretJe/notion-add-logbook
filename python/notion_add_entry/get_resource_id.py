2c0619aebed1413ab976c3952c2702f903067cb2
DucretJe <jdiphone34@gmail.com>
2022-07-16 17:51:09 +0200

    Lint

--- python/notion_add_entry/get_resource_id.py
+++ python/notion_add_entry/get_resource_id.py
@@ -2,26 +2,23 @@
 
 import requests
 
+
 def get_id(token, name):
-    '''
+    """
     Get ID of a Notion resource authenticating with given token (arg1)
     It uses a query (arg2)
     It uses filter (arg3)
-    '''
+    """
     url = "https://api.notion.com/v1/search"
     payload = {
         "page_size": 4,
         "query": name,
-        "filter": {
-            "value": "database",
-            "property": "object"
-            },
-        }
+        "filter": {"value": "database", "property": "object"},
     headers = {
         "Accept": "application/json",
         "Notion-Version": "2022-06-28",
         "Content-Type": "application/json",
-        "Authorization": "Bearer " + token
+        "Authorization": "Bearer " + token,
     }
     r = requests.post(url, json=payload, headers=headers)
     if len(r.json()["results"]) > 1:
